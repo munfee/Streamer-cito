700cff0224c1d3b5bd3c15e326d098e6017ccce7
Philippe Allard Ouellet <philippe_allard_ouellet@hotmail.com>
2021-07-26 13:24:17 -0400

    env variables added

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const path = require('path')
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/streamercito', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/streamercito', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
