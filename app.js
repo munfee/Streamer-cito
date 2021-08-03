b25b661cb95d2e88ca14ea9933b964407fb73b67
Philippe Allard Ouellet <philippe_allard_ouellet@hotmail.com>
2021-08-03 10:39:24 -0400

    added TODOs for future features

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use( express.static(path.join(__dirname, 'public')));
 app.use('/', express.static(path.join(__dirname, 'views', 'output')))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); // TODO USER PROFILE ROUTES
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
